48eb2e60a79f4d17878d4cee749b275d050345c2
dinottodebora <dinottodebora@gmail.com>
2026-09-13 21:27:39 -0700

    Set up headers, includes, libraries

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "markov.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    std::cout << "Hello!";
+}
---
+++ markov.cpp
@@ -0,0 +1,25 @@
+#include "markov.h"
+#include <fstream>
+#include <cstdlib>
+
+std::string joinWords(const std::string words[], int startIndex, int count){
+    return 0;
+}
+
+int readWordsFromFile(std::string filename, std::string words[], int maxWords){
+    return 0;
+}
+
+std::string getRandomSuffix(const std::string prefixes[], const std::string suffixes[],
+int chainSize, std::string currentPrefix){
+    return 0;
+}
+
+std::string getRandomPrefix(const std::string prefixes[], int chainSize){
+    return 0;
+}
+
+std::string generateText(const std::string prefixes[], const std::string suffixes[],
+int chainSize, int order, int numWords){
+    return 0;
+}
