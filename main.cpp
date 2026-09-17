b9c20774f0acf9e7c91914d88438d4eb4ba984ed
dinottodebora <dinottodebora@gmail.com>
2026-09-16 22:10:40 -0700

    Set up of joinWords and  readWordsFromFile, both tested in main and working

--- main.cpp
+++ main.cpp
@@ -4,5 +4,16 @@
 #include <ctime>
 
 int main() {
-    std::cout << "Hello!";
+    //std::cout << "Hello!";
+    //std::string testWords[] = {"the", "cat", "sat", "down"};
+    //std::cout << joinWords(testWords, 0, 2) << std::endl; 
+    //std::cout << joinWords(testWords, 1, 3) << std::endl;
+
+//std::string words[1000];
+//int count = readWordsFromFile("test.txt", words, 1000);
+//std::cout << "Read " << count << " words" << std::endl;
+//for (int i = 0; i < 10 && i < count; i++) {
+    //std::cout << words[i] << std::endl;
+
+}
 }
--- markov.cpp
+++ markov.cpp
@@ -3,15 +3,39 @@
 #include <cstdlib>
 
 std::string joinWords(const std::string words[], int startIndex, int count){
-    return "";
+
+    std:: string result_joint_words = "";
+    
+    for(int i = 0; i <= count-1; i++){
+        result_joint_words = result_joint_words + words[startIndex + i];
+        if (i != count - 1){
+            result_joint_words = result_joint_words + " ";   
+        }
+    }
+    return result_joint_words;
 }
 
 int readWordsFromFile(std::string filename, std::string words[], int maxWords){
-    return 0;
+    
+    std::ifstream input_File(filename);
+
+    if(input_File.is_open() == false){
+        return -1;
+    }
+    int counter = 0;
+
+    while(counter < maxWords && input_File >> words[counter]){
+        counter = counter + 1;
+    }
+
+    input_File.close();
+
+    return counter;
 }
 
 std::string getRandomSuffix(const std::string prefixes[], const std::string suffixes[],
 int chainSize, std::string currentPrefix){
+
     return "";
 }
 
